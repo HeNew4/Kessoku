4f11acec5998a86289034b7357fd3c42f48b454c
HeNew <angelmaclovio@yahoo.com>
2024-04-13 23:14:20 -0600

    Insert SQL sentence

---
+++ src/main/java/org/henew/queryBuilder/Insert.java
@@ -0,0 +1,40 @@
+package org.henew.queryBuilder;
+
+public class Insert
+{
+    private static final String INSERT_INTO_PREFIX = "INSERT INTO ";
+    private static final String VALUES_PREFIX = " VALUES ";
+    private static final String SELECT_PREFIX = " SELECT ";
+    private static final String FROM_PREFIX = " FROM ";
+
+    private final Concatenate concatenate;
+
+    public Insert() {
+        this.concatenate = new Concatenate();
+    }
+
+    public String insert(String table, String[] columns, String[]... valuesList) {
+        return buildInsertQuery(table, concatenate.brackets(columns), concatenate.brackets(valuesList));
+    }
+
+    public String insertSimple(String table, String[]... valuesList) {
+        return buildInsertQuery(table, null, concatenate.brackets(valuesList));
+    }
+
+    public String insertCopy(String sourceTable, String destinationTable) {
+        return INSERT_INTO_PREFIX + sourceTable + SELECT_PREFIX + "*" + FROM_PREFIX + destinationTable;
+    }
+
+    public String insertCopy(String sourceTable, String destinationTable, String... columns) {
+        return INSERT_INTO_PREFIX + sourceTable + concatenate.brackets(columns) + SELECT_PREFIX + concatenate.simple(columns) + FROM_PREFIX + destinationTable;
+    }
+
+    private String buildInsertQuery(String table, String columns, String values) {
+        String query = INSERT_INTO_PREFIX + table;
+        if (columns != null) {
+            query += columns;
+        }
+        query += VALUES_PREFIX + values;
+        return query;
+    }
+}
